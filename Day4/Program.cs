1bdf26d5659bb527a1655425a531d2fd3a521e60
Travis Harris <travis.harris@cfainstitute.org>
2017-12-04 12:32:37 -0500

    finished Day4

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Internal;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,21 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            //string[] test = new string[]{"aa", "bb", "cc", "dd", "ee"};
+            //string[] test1 = new string[] { "aa", "bb", "cc", "dd", "aa" };
+            //string[] test2 = new string[] { "aa", "bb", "cc", "dd", "aaa" };
+
+            //var passPhrases = new List<string[]>(){test, test1, test2 };
             var passPhrases = LoadPuzzle(@"..\Input\Day4Input.txt");
-            var acceptablePassPhrases = CheckCharacters(passPhrases);
+            //var acceptablePassPhrases = CheckCharacters(passPhrases);
+            var acceptablePassPhrases = new List<string[]>();
+            foreach (var phrase in passPhrases)
+            {
+                if (!CheckPhrase(phrase))
+                {
+                    acceptablePassPhrases.Add(phrase);
+                }
+            }
 
             Console.WriteLine("Was " + passPhrases.Count + " now " + acceptablePassPhrases.Count);
             Console.ReadKey();
@@ -49,6 +63,31 @@ namespace Day4
             }
 
             return goodChars;
+        }
+
+        public static bool CheckPhrase(string[] phrases)
+        {
+            bool remove = false;
+            for (int i = 0; i < phrases.Length; i++)
+            {
+                var checkPhraseArray = phrases[i].ToCharArray();
+                Array.Sort(checkPhraseArray);
+                var checkPhrase = new string(checkPhraseArray);
+                for (int j = i + 1; j < phrases.Length; j++)
+                {
+                    var internalPhraseArray = phrases[j].ToCharArray();
+                    Array.Sort(internalPhraseArray);
+                    var internalPhrase = new string(internalPhraseArray);
+                    if (checkPhrase == internalPhrase)
+                    {
+                        remove = true;
+                        break;
+                    }
+                }
+                if (remove)
+                    break;
+            }
+            return remove;
         }
 
     }
