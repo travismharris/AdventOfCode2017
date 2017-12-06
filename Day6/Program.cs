2f8b3ab11f6980419fa8d55420b43a083ee6a330
Travis Harris <travismharris@yahoo.com>
2017-12-06 07:35:34 -0500

    struggling on day6

---
+++ Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalArray = new int[] { 4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3 };
+            var trackList = new List<int[]>();
+            trackList.Add(originalArray);
+        }
+
+        public static int[] ShiftAndCapture(int[] array)
+        {
+            var max = array.Max();
+            var dupes = array.Where(a == max);
+        }
+
+
+    }
+}
