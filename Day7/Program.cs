8053a164da6a7fb314b99d1fb36e5ac90b3a68d2
Travis Harris <travismharris@yahoo.com>
2017-12-07 06:32:39 -0500

    started day7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -10,6 +10,39 @@ namespace Day7
     {
         static void Main(string[] args)
         {
+            //var rawTowerStringList = LoadPuzzle(@"..\Input\Day7Input.txt");
+            var rawTowerStringList = LoadPuzzle(@"..\Input\TestInput.txt");
+
+            Console.Write("There are " + rawTowerStringList.Count + " towers in the List");
+            var towers = new List<Tower>();
+            foreach(var a in rawTowerStringList)
+            {
+                towers.Add(new Tower(a));
+            }
+
+
+            bool noChanges = false;
+
+            while (!noChanges)
+            {
+
+            }
+
+            Console.Write("\n\nPress any key to exit");
+            Console.ReadKey();
         }
+
+        public static List<string[]> LoadPuzzle(string fileName)
+        {
+            List<string[]> towers = new List<string[]>();
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            foreach (var line in lines)
+            {
+                var a = line.Split();
+                towers.Add(a);
+            }
+            return towers;
+        }
+
     }
 }
---
+++ Day7/Tower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public class Tower
+    {
+        private string name;
+        private int weight;
+        private List<Tower> towersISupport = new List<Tower>();
+        private bool isSupporting;
+
+        public Tower(string[] rawTowerData)
+        {
+            this.name = rawTowerData[0];
+            AssignWeight(rawTowerData[1]);
+            if (rawTowerData.Length > 2)
+            {
+                this.isSupporting = true;
+                for (int i = 3; i < rawTowerData.Length; i++)
+                    AssignTowersISupport(rawTowerData[i]);
+            }
+
+        }
+
+        public Tower(string name)
+        {
+            this.name = name;
+        }
+
+        public void AssignWeight(string weight)
+        {
+            weight = weight.Remove(weight.IndexOf('('), 1);
+            weight = weight.Remove(weight.IndexOf(')'), 1);
+            
+            this.weight = int.Parse(weight);
+        }
+
+        public void AssignTowersISupport(string tower)
+        {
+            towersISupport.Add(new Tower(tower));
+        }
+
+    }
+}
